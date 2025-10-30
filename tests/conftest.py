d90d2a7425108735053ed009bcb3059b89579c59
Stas Chernyi <itsmeprivet@mail.ru>
2025-10-30 20:00:21 +0300

    create funcs in utils.py and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,34 @@
 import pytest
-from main import Product
-from main import Category
+
+from main import Category, Product
 
 
 @pytest.fixture()
-def product1():
+def product1() -> Product:
     return Product(
         name="Samsung Galaxy S23 Ultra", description="256GB, Серый цвет, 200MP камера", price=180000.0, quantity=5
     )
 
 
 @pytest.fixture()
-def product2():
+def product2() -> Product:
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 
 
 @pytest.fixture()
-def product3():
+def product3() -> Product:
     return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
 
 @pytest.fixture()
-def category1(product1, product2, product3):
+def product4() -> Product:
+    return Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+
+
+@pytest.fixture()
+def category1(product1: Product, product2: Product, product3: Product) -> Category:
+    Category.category_count = 0
+    Category.product_count = 0
     return Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, " "но и получения дополнительных функций для удобства жизни",
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 from main import Category, Product
 
-def test_product_init(product1, product2, product3):
+
+def test_product_init(product1: Product, product2: Product, product3: Product) -> None:
     assert product1.name == "Samsung Galaxy S23 Ultra"
     assert product1.description == "256GB, Серый цвет, 200MP камера"
     assert product1.price == 180000.0
@@ -16,49 +17,26 @@ def test_product_init(product1, product2, product3):
     assert product3.price == 31000.0
     assert product3.quantity == 14
 
-def test_product_types(product1):
+
+def test_product_types(product1: Product) -> None:
     assert isinstance(product1.name, str)
     assert isinstance(product1.description, str)
     assert isinstance(product1.price, float)
     assert isinstance(product1.quantity, int)
 
-# def test_product_count_increment():
-#     Category.category_count = 0
-#     Category.product_count = 0
-#
-#     products_1 = [
-#         Product("Product 1", "Desc 1", 100.0, 1),
-#         Product("Product 2", "Desc 2", 200.0, 2),
-#     ]
-#     products_2 = [
-#         Product("Product 3", "Desc 3", 300.0, 3),
-#     ]
-#
-#     c1 = Category("Смартфоны", "Описание", products_1)
-#     c2 = Category("Планшеты", "Описание", products_2)
-#
-#     assert Category.product_count == 3
 
-# def test_category_count_increment():
-#     # Сброс счетчика перед тестом
-#     Category.category_count = 0
-#     Category.product_count = 0
-#
-#     c1 = Category("Смартфоны", "Описание", [])
-#     c2 = Category("Планшеты", "Описание", [])
-#
-#     assert Category.category_count == 2
+def test_category_and_product_count(category1: Category, product4: Product) -> None:
+    Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
 
-# def test_product_count_with_empty_category():
-#     Category.category_count = 0
-#     Category.product_count = 0
-#
-#     c1 = Category("Пустая", "Нет продуктов", [])
-#
-#     assert Category.category_count == 1
-#     assert Category.product_count == 0
+    assert Category.category_count == 2
+    assert Category.product_count == 4
 
-def test_category_init(category1):
+
+def test_category_init(category1: Category, product1: Product, product2: Product, product3: Product) -> None:
     assert category1.name == "Смартфоны"
     assert category1.description == (
         "Смартфоны, как средство не только коммуникации, " "но и получения дополнительных функций для удобства жизни"
@@ -67,3 +45,4 @@ def test_category_init(category1):
 
     assert category1.category_count == 1
     assert category1.product_count == 3
+    assert category1.products == [product1, product2, product3]
