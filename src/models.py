d737df036eb0f5b310fc8a88532b7d46234ad539
Stas Chernyi <itsmeprivet@mail.ru>
2025-11-19 21:17:28 +0300

    added custom class exception

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from typing import Any, List, Optional
 
 from src.base_product import BaseOrderCategory, BaseProduct
+from src.exception import ZeroProductCount
 from src.print_mixin import PrintMixin
 
 
@@ -129,7 +130,17 @@ class Category(BaseOrderCategory):
     def __init__(self, name: str, description: str, products: List[Product]):
         self.name = name
         self.description = description
-        self.__products = products
+        try:
+            if products == 0:
+                raise ZeroProductCount("Нельзя добавлять товар с нулевым количеством.")
+        except ZeroProductCount as e:
+            print(str(e))
+        else:
+            self.__products = products
+            print("Товар успешно добавлен.")
+        finally:
+            print("Обработка добавления товара завершена.")
+
         Category.category_count += 1
         Category.product_count += sum(p.quantity for p in products)
 
@@ -175,7 +186,18 @@ class Order(BaseOrderCategory):
     """Класс представляющий заказ"""
 
     def __init__(self, product: "Product", quantity: int):
-        self.product = product
+        """Конструктор класса представляющий заказ"""
+        try:
+            if product == 0:
+                raise ZeroProductCount("Нельзя добавлять товар с нулевым количеством.")
+        except ZeroProductCount as e:
+            print(str(e))
+        else:
+            self.product = product
+            print("Товар успешно добавлен.")
+        finally:
+            print("Обработка добавления товара завершена.")
+
         self.quantity = quantity
         self.total_price = product.price * quantity
 
@@ -206,24 +228,3 @@ class ProductIterator:
         else:
             raise StopIteration
 
-
-if __name__ == "__main__":
-    try:
-        product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
-    except ValueError as e:
-        print(
-            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством"
-        )
-    else:
-        print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
-
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
-
-    print(category1.middle_price())
-
-    category_empty = Category("Пустая категория", "Категория без продуктов", [])
-    print(category_empty.middle_price())
